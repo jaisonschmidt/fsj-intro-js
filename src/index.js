3cb03137a15005f1d33e3e69c908ebbd6201423f
Jaison Schmidt <jaison@dogandponystudios.com>
2020-09-15 21:58:38 -0300

    Finaliza lista de albuns

--- src/index.js
+++ src/index.js
@@ -3,3 +3,26 @@ import PageAlbuns from "./pages/Albuns";
 import "./assets/css/global.css";
 
 PageAlbuns();
+
+/*
+const pessoa = {
+  nome: "Jaison",
+  idade: 36,
+  endereco: "Passo Fundo",
+  pais: "Brasil",
+  nacionalidade: "brasileiro",
+};
+
+function recebePessoa({ nome = "", idade = "", ...props }) {
+  console.log(nome + " " + idade);
+
+  console.log(`${nome == "" ? "fulano" : nome} ${idade}`);
+}
+
+recebePessoa(pessoa);
+
+
+const [variavel, setVariavel] = setState() spread
+
+
+*/
--- src/pages/Albuns/index.js
+++ src/pages/Albuns/index.js
@@ -1,6 +1,13 @@
 import createAlbumList from "../../components/AlbumList";
-
 import * as albumService from "../../service/albumService";
+
+export const renderInputSearch = function renderInputSearch(initialValue = "") {
+  // retornar um input
+  const inputSearch = document.createElement("input");
+  inputSearch.value = initialValue;
+  inputSearch.setAttribute("placeholder", "Buscar álbum");
+  return inputSearch;
+};
 
 const PageAlbuns = async () => {
   try {
@@ -8,10 +15,23 @@ const PageAlbuns = async () => {
 
     const albumListContainer = document.createElement("div");
     const albumList = createAlbumList(allAlbuns);
+    const inputSearch = renderInputSearch();
+
+    inputSearch.onkeyup = function searchAlbum(e) {
+      console.log(e.target.value);
+      albumListContainer.innerHTML = "";
+
+      let albunsFilter = allAlbuns.filter((album) =>
+        album.title.includes(e.target.value)
+      );
+
+      albumListContainer.appendChild(createAlbumList(albunsFilter));
+    };
 
     albumListContainer.append(albumList);
 
-    document.body.append(albumListContainer);
+    document.body.appendChild(inputSearch);
+    document.body.appendChild(albumListContainer);
   } catch (error) {
     console.log("Deu ruim!", error);
   }
